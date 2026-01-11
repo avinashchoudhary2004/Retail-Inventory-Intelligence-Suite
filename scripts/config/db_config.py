205dc60aa38b6fd0e475e5bccc82a25c55810255
avinashchoudhary2004 <avinashchoudhary20040@gmail.com>
2026-01-11 11:27:36 +0530

    Added Tableau Dashboard Files

--- scripts/config/db_config.py
+++ scripts/config/db_config.py
@@ -1,10 +1,9 @@
 # TEMPLATE: Rename this file to 'db_config.py' and update values
 # DO NOT COMMIT YOUR REAL PASSWORD TO GITHUB
 DB_CONFIG = {
-    "user": "postgres",
-    # "password": "CHANGE_ME",
-    "password":"mahi22",
+    "user": "CHANGE_ME",
+    "password": "CHANGE_ME",
     "host": "localhost",
-    "port": "5432",
+    "port": "CHANGE_ME",
     "dbname": "stocksmart_dw"
 }
